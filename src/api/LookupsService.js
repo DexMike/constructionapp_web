9f04bff55591c7d3a3f8fbd6d93e1a2a5fccc06d
Ammad <ammad@trelar.net>
2019-02-24 20:01:15 -0600

    SG-26 Adding LookupService

---
+++ src/api/LookupsService.js
@@ -0,0 +1,34 @@
+// consider using this type of file (independent from AgentService)
+// in order to keep al files separated
+import AgentService from './AgentService';
+
+const PATH = '/lookups';
+
+class LookupsService extends AgentService {
+  static async getLookups() {
+    const response = await super.get(PATH);
+    return (response);
+  }
+
+  static async getLookupsById(id) {
+    const response = await super.get(PATH, id);
+    return (response);
+  }
+
+  static async createLookup(lookup) {
+    const response = await super.post(PATH, lookup);
+    return (response);
+  }
+
+  static async updateLookup(lookup) {
+    const response = await this.put(PATH, lookup);
+    return (response);
+  }
+
+  static async deleteLookupById(id) {
+    const response = await this.delete(PATH, id);
+    return (response);
+  }
+}
+
+export default LookupsService;
