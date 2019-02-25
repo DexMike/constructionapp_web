4851da36a1d7e38c5eeb031e236df494ae3ff484
Ammad <ammad@trelar.net>
2019-02-24 20:02:18 -0600

    SG-26 Added dropdown support (kind of) to filter, added more formatting to truck row.

--- src/components/dashboard/DashboardCustomerPage.js
+++ src/components/dashboard/DashboardCustomerPage.js
@@ -1,23 +1,83 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
-import { Card, CardBody, Col, Container, Modal, Row } from 'reactstrap';
+import {
+  Card,
+  CardBody,
+  Col,
+  Container,
+  Modal,
+  Row
+} from 'reactstrap';
 // import classnames from 'classnames';
 import moment from 'moment';
+import { Select } from '@material-ui/core';
 import TTable from '../common/TTable';
 // import JobsService from '../../api/JobsService';
 // import AgentService from '../../api/AgentService';
 import EquipmentService from '../../api/EquipmentService';
+import LookupsService from '../../api/LookupsService';
 import JobCreateForm from '../jobs/JobCreateForm';
+
+import truckImage from '../../img/default_truck.png';
 
 class DashboardCustomerPage extends Component {
   constructor(props) {
     super(props);
 
+    // copied from Nimda EquipmentForm
+    // const equipment = {
+    //   name: '',
+    //   type: 0,
+    //   styleId: 0,
+    //   maxCapacity: 0,
+    //   minCapacity: 0,
+    //   minHours: 0,
+    //   maxDistance: 0,
+    //   description: '',
+    //   licensePlate: '',
+    //   vin: '',
+    //   image: '',
+    //   currentAvailability: 0,
+    //   hourRate: 0,
+    //   tonRate: 0,
+    //   rateType: 'Hour',
+    //   companyId: 0,
+    //   defaultDriverId: 0,
+    //   driverEquipmentsId: 0,
+    //   driversId: 0,
+    //   equipmentAddressId: 0,
+    //   modelId: '',
+    //   makeId: '',
+    //   notes: '',
+    //   createdBy: 0,
+    //   createdOn: moment().unix() * 1000,
+    //   modifiedBy: 0,
+    //   modifiedOn: moment().unix() * 1000,
+    //   isArchived: 0
+    // };
+
+    // const filter = {
+    //   startDate: null,
+    //   endDate: null,
+    //   truckType: null,
+    //   minCapactiy: null,
+    //   materials: null,
+    //   zipCode: null,
+    //   rateType: null
+    // };
+
     this.state = {
+      // companyName: '',
+      // drivers: [],
+      equipmentTypes: [],
+      materialTypes: [],
+      rateTypes: [],
+      // users: [],
       equipments: [],
       goToDashboard: false,
       selectedEquipment: {},
       modal: false
+      // ...equipment
       // goToAddJob: false,
       // goToUpdateJob: false,
       // goToCreateJob: false,
@@ -25,7 +85,6 @@ class DashboardCustomerPage extends Component {
     };
 
     this.renderGoTo = this.renderGoTo.bind(this);
-    // this.handleJobEdit = this.handleJobEdit.bind(this);
     this.handleEquipmentEdit = this.handleEquipmentEdit.bind(this);
     this.toggleAddJobModal = this.toggleAddJobModal.bind(this);
   }
@@ -35,9 +94,66 @@ class DashboardCustomerPage extends Component {
     await this.fetchEquipments();
   }
 
+  // componentWillReceiveProps(nextProps) {
+  componentWillReceiveProps() {
+    // if (nextProps.equipment) {
+    //   const { equipment } = nextProps;
+    //   Object.keys(equipment)
+    //     .map((key) => {
+    //       if (equipment[key] === null) {
+    //         equipment[key] = '';
+    //       }
+    //       return true;
+    //     });
+    //   this.setState({ ...equipment });
+    this.fetchForeignValues();
+    this.fetchParentValues();
+    // }
+  }
+
   getState() {
     const status = this.state;
     return status;
+  }
+
+  async fetchForeignValues() {
+    // const { equipment } = this.props;
+    // if (equipment.companyId) {
+    //   const company = await AgentService.getCompanyById(equipment.companyId);
+    //   this.setState({ companyName: company.legalName });
+    // }
+    //
+    const lookups = await LookupsService.getLookups();
+
+    const equipmentTypes = [];
+    Object.values(lookups).forEach((itm) => {
+      if (itm.key === 'EquipmentType') equipmentTypes.push(itm);
+    });
+    this.setState({ equipmentTypes });
+
+    const materialTypes = [];
+    Object.values(lookups).forEach((itm) => {
+      if (itm.key === 'MaterialType') materialTypes.push(itm);
+    });
+    this.setState({ materialTypes });
+
+    const rateTypes = [];
+    Object.values(lookups).forEach((itm) => {
+      if (itm.key === 'RateType') rateTypes.push(itm);
+    });
+    this.setState({ rateTypes });
+  }
+
+  async fetchParentValues() {
+    // const users = await AgentService.getUsers();
+    // const { companyId } = this.state;
+    // const drivers = [];
+    // Object.values(users).forEach((itm) => {
+    //   if (itm.companyId === companyId) {
+    //     drivers.push(itm);
+    //   }
+    // });
+    // this.setState({ drivers });
   }
 
   // async fetchJobs() {
@@ -76,6 +192,14 @@ class DashboardCustomerPage extends Component {
   //     jobId: id
   //   });
   // }
+
+  handleInputChange(e) {
+    let { value } = e.target;
+    if (e.target.name === 'isArchived') {
+      value = e.target.checked ? Number(1) : Number(0);
+    }
+    this.setState({ [e.target.name]: value });
+  }
 
   handlePageClick(menuItem) {
     if (menuItem) {
@@ -121,14 +245,14 @@ class DashboardCustomerPage extends Component {
 
   renderModal() {
     const {
-      equipments,
-      startAvailability,
-      endAvailability,
-      truckType,
-      minCapacity,
-      materials,
-      zipCode,
-      rateType,
+      // equipments,
+      // startAvailability,
+      // endAvailability,
+      // truckType,
+      // minCapacity,
+      // materials,
+      // zipCode,
+      // rateType,
       modal,
       selectedEquipment
     } = this.state;
@@ -148,6 +272,7 @@ class DashboardCustomerPage extends Component {
       </Modal>
     );
   }
+
   renderBreadcrumb() {
     return (
       <div>
@@ -160,6 +285,7 @@ class DashboardCustomerPage extends Component {
       </div>
     );
   }
+
   renderTitle() {
     return (
       <Row>
@@ -169,143 +295,215 @@ class DashboardCustomerPage extends Component {
       </Row>
     );
   }
+
   renderFilter() {
     const {
-      equipments,
       startAvailability,
       endAvailability,
-      truckType,
+      // truckType,
       minCapacity,
-      materials,
+      // materials,
       zipCode,
-      rateType,
-      modal,
-      selectedEquipment
+      // rateType,
+
+      equipmentTypes,
+      materialTypes,
+      rateTypes
+      // modal,
+      // equipments,
+      // selectedEquipment
     } = this.state;
-    return (
-        <Row>
-          <Col md={12}>
-            <Card>
-              <CardBody>
-                <h3>Filters</h3>
 
-                <form className="form" onSubmit={e => this.saveCompany(e)}>
-                  {/*<Row style={{color:Tomato}}>*/}
-                  <Row>
-                    <Col>
-                      <span className="form__form-group-label">Start Availability</span>
-                      <div className="form__form-group-field">
-                        <input name="startAvailability"
-                               type="text"
-                               placeholder="Select Start Date"
-                               value={startAvailability}
-                               onChange={this.handleInputChange}
-                        />
-                      </div>
-                    </Col>
-                    <Col>
-                      <span className="form__form-group-label">End Availability</span>
-                      <div className="form__form-group-field">
-                        <input name="endAvailability"
-                               style={{ width: '100%' }}
-                               type="text"
-                               placeholder="Select End Date"
-                               value={endAvailability}
-                               onChange={this.handleInputChange}
-                        />
-                      </div>
-                    </Col>
-                    <Col>
-                      <span className="form__form-group-label">Truck Type</span>
-                      <div className="form__form-group-field">
-                        <input name="truckType"
-                               type="text"
-                               placeholder="Any"
-                               value={truckType}
-                               onChange={this.handleInputChange}
-                        />
-                      </div>
-                    </Col>
-                    <Col>
-                      <span className="form__form-group-label">Min Capacity</span>
-                      <div className="form__form-group-field">
-                        <input name="minCapacity"
-                               type="text"
-                               placeholder="Min # of tons"
-                               value={minCapacity}
-                               onChange={this.handleInputChange}
-                        />
-                      </div>
-                    </Col>
-                    <Col>
-                      <span className="form__form-group-label">materials</span>
-                      <div className="form__form-group-field">
-                        <input name="materials"
-                               type="text"
-                               placeholder="Any"
-                               value={materials}
-                               onChange={this.handleInputChange}
-                        />
-                      </div>
-                    </Col>
-                    <Col>
-                      <span className="form__form-group-label">Zip Code</span>
-                      <div className="form__form-group-field">
-                        <input name="zipCode"
-                               type="text"
-                               placeholder="Zip Code"
-                               value={zipCode}
-                               onChange={this.handleInputChange}
-                        />
-                      </div>
-                    </Col>
-                    <Col>
-                      <span className="form__form-group-label">Rate Type</span>
-                      <div className="form__form-group-field">
-                        <input name="rateType"
-                               type="text"
-                               placeholder="Any"
-                               value={rateType}
-                               onChange={this.handleInputChange}
-                        />
-                      </div>
-                    </Col>
-                  </Row>
-                </form>
-
-              </CardBody>
-            </Card>
-          </Col>
-
-        </Row>
-      );
-  }
-
-  renderTable() {
-    const {
-      equipments,
-      startAvailability,
-      endAvailability,
-      truckType,
-      minCapacity,
-      materials,
-      zipCode,
-      rateType,
-      modal,
-      selectedEquipment
-    } = this.state;
     return (
       <Row>
         <Col md={12}>
           <Card>
             <CardBody>
-              {/*Displaying {*/}
-              {/*if equipement !=== null {*/}
-              {/*equipment.length*/}
-              {/*} else {*/}
-              {/*none*/}
-              {/*}*/}
-              {/*} of YYY*/}
+              <h3>Filters</h3>
+
+              <form className="form" onSubmit={e => this.saveCompany(e)}>
+                {/* <Row style={{color:Tomato}}> */}
+                <Row>
+                  <Col>
+                    <span className="form__form-group-label">Start Availability</span>
+                    <div className="form__form-group-field">
+                      <input name="startAvailability"
+                             type="text"
+                             placeholder="Select Start Date"
+                             value={startAvailability}
+                             onChange={this.handleInputChange}
+                      />
+                    </div>
+                  </Col>
+                  <Col>
+                    <span className="form__form-group-label">End Availability</span>
+                    <div className="form__form-group-field">
+                      <input name="endAvailability"
+                             style={{ width: '100%' }}
+                             type="text"
+                             placeholder="Select End Date"
+                             value={endAvailability}
+                             onChange={this.handleInputChange}
+                      />
+                    </div>
+                  </Col>
+                  <Col>
+                    <span className="form__form-group-label">Truck Type</span>
+                    <div className="form__form-group-field">
+                      <Select
+                        name="equipmentTypes"
+                        value={equipmentTypes}
+                        onChange={this.handleInputChange}
+                      >
+                        {
+                          equipmentTypes.map(typeSelect => (
+                            <option key={typeSelect.order} value={typeSelect.order}>
+                              {typeSelect.val1}
+                            </option>
+                          ))
+                        }
+                      </Select>
+                    </div>
+                  </Col>
+                  <Col>
+                    <span className="form__form-group-label">Min Capacity</span>
+                    <div className="form__form-group-field">
+                      <input name="minCapacity"
+                             type="text"
+                             placeholder="Min # of tons"
+                             value={minCapacity}
+                             onChange={this.handleInputChange}
+                      />
+                    </div>
+                  </Col>
+                  <Col>
+                    <span className="form__form-group-label">Materials</span>
+                    <div className="form__form-group-field">
+                      <Select
+                        name="materialTypes"
+                        value={materialTypes}
+                        onChange={this.handleInputChange}
+                      >
+                        {
+                          materialTypes.map(typeSelect => (
+                            <option key={typeSelect.order} value={typeSelect.order}>
+                              {typeSelect.val1}
+                            </option>
+                          ))
+                        }
+                      </Select>
+                    </div>
+                  </Col>
+                  <Col>
+                    <span className="form__form-group-label">Zip Code</span>
+                    <div className="form__form-group-field">
+                      <input name="zipCode"
+                             type="text"
+                             placeholder="Zip Code"
+                             value={zipCode}
+                             onChange={this.handleInputChange}
+                      />
+                    </div>
+                  </Col>
+                  <Col>
+                    <span className="form__form-group-label">Rate Type</span>
+                    <div className="form__form-group-field">
+                      <Select
+                        name="rateTypes"
+                        value={rateTypes}
+                        onChange={this.handleInputChange}
+                      >
+                        {
+                          rateTypes.map(typeSelect => (
+                            <option key={typeSelect.order} value={typeSelect.order}>
+                              {typeSelect.val1}
+                            </option>
+                          ))
+                        }
+                      </Select>
+                    </div>
+                  </Col>
+                </Row>
+              </form>
+
+            </CardBody>
+          </Card>
+        </Col>
+
+      </Row>
+    );
+  }
+
+  renderEquipmentRow(equipment) {
+    return (
+      <React.Fragment>
+        <div style={{ paddingTop: '10px' }} className="row">
+          <div className="col-sm-3">
+            <img width="100" height="85" src={`${window.location.origin}/${truckImage}`} alt=""
+                 style={{ width: '100px' }}
+            />
+          </div>
+          <div className="col-sm-3">
+            <span>
+              {equipment.type}
+              {equipment.maxCapacity}
+              {equipment.hourRate}
+
+              Rate/hour Minimum ton
+
+              {equipment.hourRate}
+              {equipment.tonRate}
+              {equipment.companyId}
+              {equipment.notes}
+              <div className="col-sm-8">
+                <button type="button" className="btn btn-primary">
+                  Request
+                </button>
+              </div>
+
+            </span>
+          </div>
+          <div className="col-sm-3">
+            <span>50 Tons</span>
+          </div>
+          <div className="col-sm-3">
+            <span>{equipment.maxCapacity}</span>
+          </div>
+          <div className="col-sm-3">
+            {/* {this.renderEquipmentMaterials()} */}
+          </div>
+        </div>
+        <hr />
+      </React.Fragment>
+    );
+  }
+
+  renderEquipmentTable() {
+    const {
+      equipments
+    } = this.state;
+
+    return (
+      <Row>
+        <Col md={12}>
+          <Card>
+            <CardBody>
+
+              Displaying&nbsp;
+              {equipments.length}
+              &nbsp;of&nbsp;
+              {equipments.length}
+
+              <Row>
+                {
+                  equipments.map(equipment => (
+                    <div key={equipment.id}>
+                      {this.renderEquipmentRow(equipment)}
+                    </div>
+                  ))
+                }
+              </Row>
+
               <TTable
                 columns={
                   [
@@ -359,18 +557,6 @@ class DashboardCustomerPage extends Component {
   }
 
   render() {
-    const {
-      equipments,
-      startAvailability,
-      endAvailability,
-      truckType,
-      minCapacity,
-      materials,
-      zipCode,
-      rateType,
-      modal,
-      selectedEquipment
-    } = this.state;
     return (
       <Container className="dashboard">
         {this.renderModal()}
@@ -378,7 +564,8 @@ class DashboardCustomerPage extends Component {
         {this.renderBreadcrumb()}
         {this.renderTitle()}
         {this.renderFilter()}
-        {this.renderTable()}
+        {/* {this.renderTable()} */}
+        {this.renderEquipmentTable()}
       </Container>
     );
   }
