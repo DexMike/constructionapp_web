0eabbf08c785b6173b7bd30bb8d1c375b31d52d4
Jake Howton <jake@trelar.net>
2019-03-06 09:36:07 -0600

    SG-74 Fixing lint error

--- src/components/dashboard/DashboardCarrierPage.js
+++ src/components/dashboard/DashboardCarrierPage.js
@@ -211,7 +211,7 @@ class DashboardCarrierPage extends Component {
             <div className="card">
               <div className="dashboard__card-widget card-body">
                 <h5 className="card__title bold-text"><center>Potential Earnings</center></h5>
-                {/*<span><center><h4>{newJobCount}</h4></center></span>*/}
+                {/* <span><center><h4>{newJobCount}</h4></center></span> */}
               </div>
             </div>
           </div>
