2366b1054967a9ce94f1f4603960ddf09676bb1e
gente21 <raulrodriguezarias@gmail.com>
2019-03-12 00:36:01 +0700

    merged SG-77 and SG-22

--- src/components/common/TDateTimePicker.js
+++ src/components/common/TDateTimePicker.js
@@ -2,44 +2,68 @@ import React, { PureComponent } from 'react';
 import DatePicker from 'react-datepicker';
 import PropTypes from 'prop-types';
 
-class TDateTimePicker extends PureComponent {
+class TDateTimePickerField extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      startDate: new Date(props.givenDate)
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(date) {
+    this.setState({
+      startDate: date
+    });
+    const { onChange } = this.props;
+    onChange(date);
+  }
+
   render() {
-    const { input, dateFormat, meta: { touched, error } } = this.props;
+    const { startDate } = this.state;
+    const { dateFormat } = this.props;
+    // console.log(input.dateFormat);
     return (
-      <div className="form__form-group-input-wrap form__form-group-input-wrap--error-above">
-        <div className="date-picker">
-          <DatePicker
-            {...input}
-            timeFormat="HH:mm"
-            className="form__form-group-datepicker"
-            // selected={startDate}
-            // showTimeSelect //shows Time picker as well
-            // onChange={this.handleChange}
-            dateFormat={dateFormat}
-          />
-        </div>
-        {touched && error && <span className="form__form-group-error">{error}</span>}
+      <div className="date-picker">
+        <DatePicker
+          timeFormat="HH:mm"
+          className="form__form-group-datepicker"
+          selected={startDate}
+          // showTimeSelect //shows Time picker as well
+          onChange={this.handleChange}
+          dateFormat={dateFormat}
+        />
       </div>
     );
   }
 }
 
-TDateTimePicker.propTypes = {
+const renderTDateTimePickerField = function renderTDateTimePickerField(props) {
+  const { input } = props;
+  return (
+    <TDateTimePickerField
+      {...input}
+    />
+  );
+};
+
+renderTDateTimePickerField.propTypes = {
+  onChange: PropTypes.func.isRequired,
   input: PropTypes.shape({
     onChange: PropTypes.func,
-    name: PropTypes.string,
-    value: PropTypes.string
-  }).isRequired,
-  meta: PropTypes.shape({
-    touched: PropTypes.bool,
-    error: PropTypes.string
-  }),
+    name: PropTypes.string
+  }).isRequired
+};
+
+TDateTimePickerField.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  givenDate: PropTypes.number,
   dateFormat: PropTypes.string
 };
 
-TDateTimePicker.defaultProps = {
-  dateFormat: 'MMMM dd, yyyy hh:mm aaa',
-  meta: { touched: false, error: '' }
+TDateTimePickerField.defaultProps = {
+  givenDate: PropTypes.number,
+  dateFormat: 'MMMM dd, yyyy hh:mm aaa'
 };
 
-export default TDateTimePicker;
+export default renderTDateTimePickerField;
