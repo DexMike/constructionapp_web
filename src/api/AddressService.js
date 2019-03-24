1977ea7117fbc7d76748ff2115550ce1af7668eb
Ammad <writer648@yahoo.com>
2019-03-23 23:16:49 -0500

    SG-130 Matching update to the Nimda changes for matching services between Nimda and Stargate

--- src/api/AddressService.js
+++ src/api/AddressService.js
@@ -1,5 +1,3 @@
-// consider using this type of file (independent from AgentService)
-// in order to keep all files separated
 import moment from 'moment';
 import AgentService from './AgentService';
 
@@ -8,6 +6,16 @@ const PATH = '/addresses';
 class AddressService extends AgentService {
   static async getAddresses() {
     const response = await super.get(PATH);
+    return (response);
+  }
+
+  static async getAddressById(id) {
+    const response = await this.get(`${PATH}/${id}`);
+    return (response);
+  }
+
+  static async getAddressesByCompanyId(id) {
+    const response = await this.get(`/companies/${id}/addresses`);
     return (response);
   }
 
@@ -23,11 +31,6 @@ class AddressService extends AgentService {
 
   static async deleteAddressbById(id) {
     const response = await this.delete(PATH, id);
-    return (response);
-  }
-
-  static async getAddressById(id) {
-    const response = await this.get(`${PATH}/${id}`);
     return (response);
   }
 
--- src/api/UserService.js
+++ src/api/UserService.js
@@ -50,13 +50,3 @@ class UserService extends AgentService {
 }
 
 export default UserService;
-
-// INSERT INTO `orion`.`Equipments` (`name`, `type`, `styleId`, `maxCapacity`,
-// `minCapacity`, `minHours`, `maxDistance`, `description`, `licensePlate`, `vin`,
-// `image`, `currentAvailability`, `hourRate`, `tonRate`, `rateType`, `companyId`,
-// `defaultDriverId`, `driverEquipmentsId`, `driversId`, `equipmentAddressId`,
-// `modelId`, `makeId`, `notes`, `createdBy`, `createdOn`, `modifiedBy`,
-// `modifiedOn`, `isArchived`) VALUES ('Truck #2', '45', '45', '25000', '20000',
-// '8', '77', 'Truck Description', 'JJJ787', 'KvinNumber', 'AWS S3 Path 3', '1',
-// '105', '45', '2', '19', '5', '1', '4', '5', '1', '2', 'Notes about this truck',
-// '5', '2019-02-22', '1', '2019-02-22', '0');
