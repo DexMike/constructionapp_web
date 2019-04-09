310c442513fa5ca0db118a5bb366c634bc0b2ab1
reza piri <reza.piri@gennovacap.com>
2019-04-09 09:42:45 -0500

    Sg-170: Styled the dashboard object

--- src/components/dashboard/DashboardObject.js
+++ src/components/dashboard/DashboardObject.js
@@ -6,7 +6,7 @@ export const DashboardObject = ({title, val}) => {
   return (
     <div className="col-12 col-md-2 col-lg-2">
       <div className="card">
-        <div className="dashboard__card-widget card-body">
+        <div className="dashboard__card-widget card-body card-filter">
           <h5 className="card__title bold-text">
             <center>{t(title)}</center>
           </h5>
