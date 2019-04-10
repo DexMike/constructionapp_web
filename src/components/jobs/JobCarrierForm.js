393255dac62dc0e033fcb445166d1b3a6937db6b
Ricardo López <rlpza.94@gmail.com>
2019-04-10 13:21:33 -0500

    SG-186 General advances

--- src/components/jobs/JobCarrierForm.js
+++ src/components/jobs/JobCarrierForm.js
@@ -346,7 +346,9 @@ class JobCarrierForm extends Component {
     const images = [
       { id: 1, name: 'Truck Image 1', image: 'http://www.overdriveonline.com/wp-content/uploads/sites/8/2016/08/KW-T880-super-dump-2016-08-01-15-51.jpg'},
       { id: 2, name: 'Truck Image 2', image: 'http://www.overdriveonline.com/wp-content/uploads/sites/8/2016/08/KW-T880-super-dump-2016-08-01-15-51.jpg'},
-      { id: 3, name: 'Truck Image 3', image: 'http://www.overdriveonline.com/wp-content/uploads/sites/8/2016/08/KW-T880-super-dump-2016-08-01-15-51.jpg'}
+      { id: 3, name: 'Truck Image 3', image: 'http://www.overdriveonline.com/wp-content/uploads/sites/8/2016/08/KW-T880-super-dump-2016-08-01-15-51.jpg'},
+      { id: 4, name: 'Truck Image 4', image: 'http://www.overdriveonline.com/wp-content/uploads/sites/8/2016/08/KW-T880-super-dump-2016-08-01-15-51.jpg'},
+      { id: 5, name: 'Truck Image 5', image: 'http://www.overdriveonline.com/wp-content/uploads/sites/8/2016/08/KW-T880-super-dump-2016-08-01-15-51.jpg'}
     ];
     const { job } = this.props;
     let origin = '';
@@ -407,18 +409,11 @@ class JobCarrierForm extends Component {
                 </Col>
               </Row>
               <Row>
-                <TTable
-                  columns={
-                    [
-                      {
-                        name: 'image',
-                        displayName: 'Truck Image'
-                      }
-                    ]
-                  }
-                  data={images}
-                  handleIdClick={this.handleImg}
-                />
+                {images.map(item => (
+                  <Col className="col-md-4 pt-4" key={`img-${item.id}`}>
+                    <img key={item.id} src={`${item.image}`} alt={`${item.name}`}/>
+                  </Col>
+                ))}
               </Row>
             </CardBody>
           </Card>
--- src/components/jobs/JobCustomerForm.js
+++ src/components/jobs/JobCustomerForm.js
@@ -337,6 +337,13 @@ class JobForm extends Component {
   }
 
   render() {
+    const images = [
+      { id: 1, name: 'Truck Image 1', image: 'http://www.overdriveonline.com/wp-content/uploads/sites/8/2016/08/KW-T880-super-dump-2016-08-01-15-51.jpg'},
+      { id: 2, name: 'Truck Image 2', image: 'http://www.overdriveonline.com/wp-content/uploads/sites/8/2016/08/KW-T880-super-dump-2016-08-01-15-51.jpg'},
+      { id: 3, name: 'Truck Image 3', image: 'http://www.overdriveonline.com/wp-content/uploads/sites/8/2016/08/KW-T880-super-dump-2016-08-01-15-51.jpg'},
+      { id: 4, name: 'Truck Image 4', image: 'http://www.overdriveonline.com/wp-content/uploads/sites/8/2016/08/KW-T880-super-dump-2016-08-01-15-51.jpg'},
+      { id: 5, name: 'Truck Image 5', image: 'http://www.overdriveonline.com/wp-content/uploads/sites/8/2016/08/KW-T880-super-dump-2016-08-01-15-51.jpg'}
+    ];
     const { job } = this.props;
     let origin = '';
     let destination = '';
@@ -397,6 +404,13 @@ class JobForm extends Component {
                     }
                   />
                 </Col>
+              </Row>
+              <Row>
+                {images.map(item => (
+                  <Col className="col-md-4 pt-4" key={`img-${item.id}`}>
+                    <img key={item.id} src={`${item.image}`} alt={`${item.name}`}/>
+                  </Col>
+                ))}
               </Row>
             </CardBody>
           </Card>
