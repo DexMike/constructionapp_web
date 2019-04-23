73ca5f5739b79a8d7bd053377dad9cd8abde5f6e
martin <martin@trelar.net>
2019-04-23 17:16:20 -0500

    SG-321 cleaning up trucks customer page component

--- src/components/trucksList/TrucksCustomerPage.js
+++ src/components/trucksList/TrucksCustomerPage.js
@@ -9,29 +9,15 @@ import {
   Modal,
   Row
 } from 'reactstrap';
-// import classnames from 'classnames';
-import moment from 'moment';
-// import { Select } from '@material-ui/core';
 import NumberFormat from 'react-number-format';
 import TSelect from '../common/TSelect';
-
-import EquipmentService from '../../api/EquipmentService';
-import LookupsService from '../../api/LookupsService';
 import JobCreateForm from '../jobs/JobCreateForm';
 import truckImage from '../../img/default_truck.png';
-// import truckImage from '../../img/belly-dump.jpg';
-import CompanyService from '../../api/CompanyService';
-import AddressService from '../../api/AddressService';
 import ProfileService from '../../api/ProfileService';
-// import JobMaterialsService from '../../api/JobMaterialsService';
-// import JobsService from '../../api/JobsService';
-// import AgentService from '../../api/AgentService';
-import MultiSelect from '../common/TMultiSelect';
-import TIntervalDatePicker from '../common/TIntervalDatePicker';
 import './Truck.css';
 import GroupService from '../../api/GroupService';
 import GroupListService from '../../api/GroupListService';
-import TruckFilter from "../filters/TruckFilter";
+import TruckFilter from '../filters/TruckFilter';
 
 class TrucksCustomerPage extends Component {
   constructor(props) {
@@ -45,47 +31,17 @@ class TrucksCustomerPage extends Component {
     // Comment
     this.state = {
       loaded: false,
-
       // Look up lists
-      equipmentTypeList: [],
       materialTypeList: [],
-      rateTypeList: [],
       sortByList,
-
       equipments: [],
       selectedEquipment: {},
-
       modal: false,
-      goToDashboard: false,
-      startDate: null,
-      endDate: null,
-
-      // TODO: Refactor to a single filter object
-      // Filter values
-      filters: {
-        startAvailability: null,
-        endAvailability: null,
-        searchType: 'Customer Truck',
-        userId: '',
-        equipmentType: '',
-        minCapacity: '',
-        // materialType: '',
-        materialType: [],
-        zipCode: '',
-        rateType: '',
-        sortBy: sortByList[0]
-      }
+      goToDashboard: false
     };
 
     this.renderGoTo = this.renderGoTo.bind(this);
     this.handleEquipmentEdit = this.handleEquipmentEdit.bind(this);
-    this.toggleAddJobModal = this.toggleAddJobModal.bind(this);
-    this.handleFilterChange = this.handleFilterChange.bind(this);
-    this.handleSelectFilterChange = this.handleSelectFilterChange.bind(this);
-    this.handleStartDateChange = this.handleStartDateChange.bind(this);
-    this.handleEndDateChange = this.handleEndDateChange.bind(this);
-    this.handleMultiChange = this.handleMultiChange.bind(this);
-    this.handleIntervalInputChange = this.handleIntervalInputChange.bind(this);
     this.returnSelectedMaterials = this.returnSelectedMaterials.bind(this);
     this.retrieveAllMaterials = this.retrieveAllMaterials.bind(this);
     this.toggleSelectMaterialsModal = this.toggleSelectMaterialsModal.bind(this);
@@ -112,128 +68,6 @@ class TrucksCustomerPage extends Component {
     return materialTypeList;
   }
 
-  async fetchFilterLists() {
-    const {filters, materialTypeList, equipmentTypeList, rateTypeList} = this.state;
-    const profile = await ProfileService.getProfile();
-
-    if (profile.companyId) {
-      const company = await CompanyService.getCompanyById(profile.companyId);
-      if (company.addressId) {
-        const address = await AddressService.getAddressById(company.addressId);
-        filters.zipCode = address.zipCode ? address.zipCode : filters.zipCode;
-      }
-    }
-
-    // TODO need to refactor above to do the filtering on the Orion
-    // LookupDao Hibernate side
-
-    const lookupEquipmentList = await LookupsService.getLookupsByType('EquipmentType');
-    Object.values(lookupEquipmentList)
-      .forEach((itm) => {
-        equipmentTypeList.push(itm.val1);
-      });
-
-    const lookupMaterialTypeList = await LookupsService.getLookupsByType('MaterialType');
-    Object.values(lookupMaterialTypeList)
-      .forEach((itm) => {
-        materialTypeList.push(itm.val1);
-      });
-
-    const lookupRateTypelist = await LookupsService.getLookupsByType('RateType');
-    Object.values(lookupRateTypelist)
-      .forEach((itm) => {
-        rateTypeList.push(itm.val1);
-      });
-
-    [filters.equipmentType] = equipmentTypeList;
-    [filters.materials] = materialTypeList;
-    [filters.rateType] = rateTypeList;
-    this.setState({
-      filters,
-      equipmentTypeList,
-      materialTypeList,
-      rateTypeList
-    });
-  }
-
-  async fetchFavoriteEquipments(equipments) {
-    // we get all groups.companyId that have name 'Favorite'
-    const groupsFavorites = await GroupListService.getGroupListsFavorites();
-
-    if (groupsFavorites) {
-      // if we find the equipment's companyId in
-      // groupsFavorites we favorite it
-      equipments.map((equipment) => {
-        const newEquipment = equipment;
-        if (groupsFavorites.includes(newEquipment.companyId)) {
-          newEquipment.favorite = true;
-        } else {
-          newEquipment.favorite = false;
-        }
-        return newEquipment;
-      });
-      this.setState({equipments});
-    }
-  }
-
-  async fetchEquipments() {
-    const {filters} = this.state;
-    const equipments = await EquipmentService.getEquipmentByFilters(filters);
-
-    if (equipments) {
-      // NOTE let's try not to use Promise.all and use full api calls
-      // Promise.all(
-
-      this.fetchFavoriteEquipments(equipments); // we fetch what equipments are favorite
-      // this.fetchEquipmentMaterials(equipments);
-
-      equipments.map((equipment) => {
-        const newEquipment = equipment;
-        //     const company = await CompanyService.getCompanyById(newEquipment.companyId);
-        //     newEquipment.companyName = company.legalName;
-        // console.log(companyName);
-        // console.log(job.companyName)
-        // const materialsList = await EquipmentMaterialsService
-        // .getEquipmentMaterialsByJobId(job.id);
-        // const materials = materialsList.map(materialItem => materialItem.value);
-        // newJob.material = this.equipmentMaterialsAsString(materials);
-        // console.log(companyName);
-        // console.log(job.material);
-        newEquipment.modifiedOn = moment(equipment.modifiedOn)
-          .format();
-        newEquipment.createdOn = moment(equipment.createdOn)
-          .format();
-        return newEquipment;
-      });
-      this.setState({equipments});
-    }
-  }
-
-  async handleFilterChange(e) {
-    const {value} = e.target;
-    const {filters} = this.state;
-    filters[e.target.name] = value;
-    this.setState({filters});
-    await this.fetchEquipments();
-  }
-
-  async handleSelectFilterChange(option) {
-    const {value, name} = option;
-    const {filters} = this.state;
-    filters[name] = value;
-    this.setState({filters});
-    await this.fetchEquipments();
-  }
-
-  handleMultiChange(data) {
-    const {filters} = this.state;
-    filters.materialType = data;
-    this.setState({
-      filters
-    }, async function changed() {
-      await this.fetchEquipments();
-    });
-  }
 
   handlePageClick(menuItem) {
     if (menuItem) {
@@ -304,7 +138,8 @@ class TrucksCustomerPage extends Component {
       filters.materialType = options;
 
       // TODO:UI replace with a better error message dialog
-      // alert('I\'m sorry but this Truck does not have any allowable materials.  Please select a truck with Materials');
+      // alert('I\'m sorry but this Truck does not have any allowable materials.
+      // Please select a truck with Materials');
       // return false;
     }
     this.setState({
@@ -312,41 +147,6 @@ class TrucksCustomerPage extends Component {
       modal: true
     });
     return true;
-  }
-
-  async handleStartDateChange(e) {
-    const {filters} = this.state;
-    filters.startAvailability = e;
-    await this.fetchEquipments();
-    this.setState({filters});
-  }
-
-  async handleEndDateChange(e) {
-    const {filters} = this.state;
-    filters.endAvailability = e;
-    await this.fetchEquipments();
-    this.setState({filters});
-  }
-
-  async handleIntervalInputChange(e) {
-    const {filters} = this.state;
-    filters.startAvailability = e.start;
-    filters.endAvailability = e.end;
-    await this.fetchEquipments();
-    this.setState({filters});
-  }
-
-  toggleAddJobModal() {
-    const {modal, filters} = this.state;
-    if (modal) {
-      filters.materialType = [];
-      this.setState({
-        filters
-      });
-    }
-    this.setState({
-      modal: !modal
-    });
   }
 
   toggleSelectMaterialsModal() {
@@ -478,163 +278,6 @@ class TrucksCustomerPage extends Component {
     );
   }
 
-  renderFilter() {
-    const {
-      // Lists
-      equipmentTypeList,
-      materialTypeList,
-      rateTypeList,
-      startDate,
-      endDate,
-
-      // filters
-      filters
-
-    } = this.state;
-
-    return (
-      <Row>
-        <Col md={12}>
-          <Card>
-            <CardBody>
-              <form id="filter-form" className="form" onSubmit={e => this.saveCompany(e)}>
-                <Col lg={12}>
-                  <Row lg={12} id="filter-input-row">
-                    <Col md="2">
-                      <div className="filter-item-title">
-                        Availability
-                      </div>
-                      <TIntervalDatePicker
-                        startDate={startDate}
-                        endDate={endDate}
-                        name="dateInterval"
-                        onChange={this.handleIntervalInputChange}
-                        dateFormat="MM/dd/yy"
-                      />
-                    </Col>
-                    <Col md="2">
-                      <div className="filter-item-title">
-                        Truck Type
-                      </div>
-                      <TSelect
-                        input={
-                          {
-                            onChange: this.handleSelectFilterChange,
-                            name: 'equipmentType',
-                            value: filters.equipmentType
-                          }
-                        }
-                        meta={
-                          {
-                            touched: false,
-                            error: 'Unable to select'
-                          }
-                        }
-                        value={filters.equipmentType}
-                        options={
-                          equipmentTypeList.map(equipmentType => ({
-                            name: 'equipmentType',
-                            value: equipmentType,
-                            label: equipmentType
-                          }))
-                        }
-                        placeholder={equipmentTypeList[0]}
-                      />
-                    </Col>
-                    <Col md="2">
-                      <div className="filter-item-title">
-                        Rate Type
-                      </div>
-                      <TSelect
-                        input={
-                          {
-                            onChange: this.handleSelectFilterChange,
-                            name: 'rateType',
-                            value: filters.rateType
-                          }
-                        }
-                        meta={
-                          {
-                            touched: false,
-                            error: 'Unable to select'
-                          }
-                        }
-                        value={filters.rateType}
-                        options={
-                          rateTypeList.map(rateType => ({
-                            name: 'rateType',
-                            value: rateType,
-                            label: rateType
-                          }))
-                        }
-                        placeholder="Select materials"
-                      />
-                    </Col>
-                    <Col md="1">
-                      <div className="filter-item-title">
-                        Min Capacity
-                      </div>
-                      <input name="minCapacity"
-                             className="filter-text"
-                             type="number"
-                             placeholder="# of tons"
-                             value={filters.minCapacity}
-                             onChange={this.handleFilterChange}
-                      />
-                    </Col>
-                    <Col md="4">
-                      <div className="filter-item-title">
-                        Materials
-                      </div>
-                      <MultiSelect
-                        input={
-                          {
-                            onChange: this.handleMultiChange,
-                            // onChange: this.handleSelectFilterChange,
-                            name: 'materialType',
-                            value: filters.materialType
-                          }
-                        }
-                        meta={
-                          {
-                            touched: false,
-                            error: 'Unable to select'
-                          }
-                        }
-                        options={
-                          materialTypeList.map(materialType => ({
-                            name: 'materialType',
-                            value: materialType.trim(),
-                            label: materialType.trim()
-                          }))
-                        }
-                        // placeholder="Materials"
-                        placeholder={materialTypeList[0]}
-                      />
-                    </Col>
-                    <Col md="1">
-                      <div className="filter-item-title">
-                        Zip Code
-                      </div>
-                      <input name="zipCode"
-                             className="filter-text"
-                             type="text"
-                             placeholder="Zip Code"
-                             value={filters.zipCode}
-                             onChange={this.handleFilterChange}
-                      />
-                    </Col>
-                  </Row>
-                </Col>
-                <br/>
-              </form>
-            </CardBody>
-          </Card>
-        </Col>
-      </Row>
-    );
-  }
-
   renderEquipmentRow(equipment) {
     let imageTruck = '';
 
@@ -668,7 +311,7 @@ class TrucksCustomerPage extends Component {
                       thousandSeparator
                       prefix=" "
                       suffix=" Tons"
-                    />
+                      />
                     </h3>
                   </div>
                   <div className="col-md-3 button-card">
@@ -856,8 +499,7 @@ class TrucksCustomerPage extends Component {
           {this.renderGoTo()}
           {this.renderTitle()}
           <div className="truck-container">
-            <TruckFilter returnEquipments={this.returnEquipments} />
-            {/*{this.renderFilter()}*/}
+            <TruckFilter returnEquipments={this.returnEquipments}/>
             {/* {this.renderTable()} */}
             {this.renderEquipmentTable()}
           </div>
