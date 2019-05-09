d456bd384e55535f3e683a20e50d9c83f721d286
Miguel Rodriguez <dexmikerdz@gmail.com>
2019-05-09 12:19:23 -0500

    SG-324: updated send email example

--- src/api/EmailService.js
+++ src/api/EmailService.js
@@ -4,13 +4,15 @@ import AgentService from './AgentService';
 const email = {
   toEmail: 'mike@trelar.net',
   toName: 'Test Subject #326',
-  // eslint-disable-next-line prefer-template
-  subject: 'Test | ' + date.getHours() + ':' + date.getMinutes(),
+  subject: `Test | ${date.getHours()}:${date.getMinutes()}`,
   isHTML: true,
   body: '<strong>BOLD TEXT!</strong><br><br>Testing...',
+  recipients: [
+    {name: 'Trelar Customer', email: 'mike@trelar.net'}
+  ],
   attachments: [
-    'https://d2x9cff48m5sea.cloudfront.net/public/2019/04/7UAf2p.jpg',
-    'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf'
+    'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf',
+    'https://d2x9cff48m5sea.cloudfront.net/public/2019/04/9nYLsF.jpg'
   ]
 };
 await EmailService.sendEmail(email); */
