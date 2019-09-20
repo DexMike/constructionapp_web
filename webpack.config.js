dc86b8944b743ede69c4a022e1e3c2548ca9fde9
Eric Engberg <eric@trelar.net>
2019-09-20 08:29:33 -0500

    Added missing dotenv library import

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 import webpack from 'webpack';
+import Dotenv from 'dotenv-webpack';
 import path from 'path';
 // import ExtractTextPlugin from 'extract-text-webpack-plugin';
 // import CompressionPlugin from 'compression-webpack-plugin';
