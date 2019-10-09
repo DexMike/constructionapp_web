0e09174f91b8fca4ffb819ebbef7e92f5e3716ad
Ricardo López <rlpza.94@gmail.com>
2019-10-09 14:42:24 -0500

    SG-752 Range filters: Setting value to 999 if range > 999 or range < 0/null

--- src/components/filters/JobDriversFilter.js
+++ src/components/filters/JobDriversFilter.js
@@ -329,7 +329,7 @@ class JobFilter extends Component {
 
   async handleFilterChangeDelayed(e) {
     const self = this;
-    const {value} = e.target;
+    let {value} = e.target;
     const {filters, reqHandlerZip, reqHandlerRange} = this.state;
     const filter = e.target.name;
     let invalidZip = false;
@@ -349,15 +349,16 @@ class JobFilter extends Component {
       invalidZip = false;
     }
 
-    if (filter === 'range' && (value.length > 3 || value < 0)) {
-      this.setState({
-        reqHandlerRange: {
-          ...reqHandlerRange,
-          error: 'Range can not be more than 999 and less than 0',
-          touched: true
-        }
-      });
-      invalidRange = true;
+    if (filter === 'range' && (value > 999 || value < 0 || value.length === 0)) {
+      value = '999';
+      // this.setState({
+      //   reqHandlerRange: {
+      //     ...reqHandlerRange,
+      //     error: 'Range can not be more than 999 and less than 0',
+      //     touched: true
+      //   }
+      // });
+      // invalidRange = true;
     } else {
       this.setState({
         reqHandlerRange: {
